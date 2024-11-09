4ce1b07d3bb480dc127539dae7083009288482f8
Junaid <jm355010@gmail.com>
2024-11-09 01:04:11 +0000

    fixed bug and error with py 3.13+

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ with open("keys.yaml", "r") as file:
     config = yaml.safe_load(file)
 
 botToken = config["discord"]["token"]
-bot = commands.Bot(intents=discord.Intents.all())
+bot = commands.Bot(command_prefix='', intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
@@ -18,6 +18,9 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    print(f"{message.author}: {message.content}\n")
+    # thats how u send a msg
+    # await message.channel.send(f"shut up you freak {message.author}")
+
+    print(f"{message.author}: {message.content}")
 
 bot.run(botToken)
